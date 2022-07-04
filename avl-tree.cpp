c59924f233a0dbd188d7f1636dafe67f12210d6c
kothiga <kothiga2112@gmail.com>
2022-07-04 14:36:29 -0400

    Added lab assignment.

---
+++ avl-tree.cpp
@@ -0,0 +1,9 @@
+#include "avl-tree.h";
+
+bool AVLTree::insert(DataType val) {
+
+}
+
+bool AVLTree::remove(DataType val) {
+    
+}
---
+++ binary-search-tree.cpp
@@ -0,0 +1,57 @@
+#include "binary-search-tree.h"
+
+BinarySearchTree::Node::Node(DataType newval) {
+
+}
+
+int BinarySearchTree::getNodeDepth(Node* n) const {
+
+}
+
+BinarySearchTree::BinarySearchTree() {
+
+}
+
+BinarySearchTree::~BinarySearchTree() {
+
+}
+
+unsigned int BinarySearchTree::size() const {
+
+}
+
+BinarySearchTree::DataType BinarySearchTree::max() const {
+
+}
+
+BinarySearchTree::DataType BinarySearchTree::min() const {
+
+}
+
+unsigned int BinarySearchTree::depth() const {
+
+}
+
+void BinarySearchTree::print() const {
+
+}
+
+bool BinarySearchTree::exists(DataType val) const {
+
+}
+
+BinarySearchTree::Node* BinarySearchTree::getRootNode() {
+
+}
+
+BinarySearchTree::Node** BinarySearchTree::getRootNodeAddress() {
+
+}
+
+bool BinarySearchTree::insert(DataType val) {
+
+}
+
+bool BinarySearchTree::remove(DataType val) {
+
+}
